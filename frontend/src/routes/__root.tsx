10e91f6c775a233a20021021a70e2a6da7e213fb
Hayden Frank <44754643+haydenfrank@users.noreply.github.com>
2026-03-21 15:35:58 -0400

    fix: Removed TanStack Start Demo Files (#2)
    * fix: removed tanstack start demo files
    
    * feat: newPage, api integration, justfile
    
    ---------
    
    Co-authored-by: Elijah De Guzman <63167131+Cloud-Multiplayer@users.noreply.github.com>

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -3,16 +3,8 @@ import {
   Scripts,
   createRootRouteWithContext,
 } from '@tanstack/react-router'
-import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
-import { TanStackDevtools } from '@tanstack/react-devtools'
-
-import Header from '../components/Header'
-
-import TanStackQueryDevtools from '../integrations/tanstack-query/devtools'
 
 import appCss from '../styles.css?url'
-
-import type { QueryClient } from '@tanstack/react-query'
 
 interface MyRouterContext {
   queryClient: QueryClient
@@ -44,26 +36,14 @@ export const Route = createRootRouteWithContext<MyRouterContext>()({
 })
 
 function RootDocument({ children }: { children: React.ReactNode }) {
+
   return (
     <html lang="en">
       <head>
         <HeadContent />
       </head>
       <body>
-        <Header />
         {children}
-        <TanStackDevtools
-          config={{
-            position: 'bottom-right',
-          }}
-          plugins={[
-            {
-              name: 'Tanstack Router',
-              render: <TanStackRouterDevtoolsPanel />,
-            },
-            TanStackQueryDevtools,
-          ]}
-        />
         <Scripts />
       </body>
     </html>
