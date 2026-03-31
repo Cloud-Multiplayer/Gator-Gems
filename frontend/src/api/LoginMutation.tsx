3d9d600eda4f5c8d2f7b149a7c12dbe8cbee90e9
Hayden Frank <44754643+haydenfrank@users.noreply.github.com>
2026-03-30 22:37:43 -0400

    fix: image imports on login and signup pages (#49)
    * fix: image imports on login and signup pages
    
    * refactor/separate-components-mutations (#53)
    
    * feat: added vitest and updated pre-commit hooks and justfile
    
    * feat: moved login page and mutation into separate files
    
    * feat: moved signup into different files
    
    * feat: login mutation uses cookies instead of local storage

---
+++ frontend/src/api/LoginMutation.tsx
@@ -0,0 +1,38 @@
+import { useMutation } from '@tanstack/react-query'
+import createClient from 'openapi-fetch'
+import { useNavigate } from '@tanstack/react-router'
+import type { paths } from '../types/api'
+
+export const useLoginMutation = (
+  emailAddress: string,
+  password: string,
+  setError: (error: string) => void,
+) => {
+  const navigate = useNavigate()
+  const client = createClient<paths>({
+    baseUrl: 'http://localhost:8000',
+  })
+
+  return useMutation({
+    mutationFn: async () => {
+      const { response } = await client.GET('/users/{email_address}/login', {
+        params: { path: { email_address: emailAddress } },
+        headers: {
+          Authorization: `Basic ${btoa(`${emailAddress}:${password}`)}`,
+        },
+        credentials: 'include',
+      })
+
+      if (!response.ok) {
+        throw new Error('Login failed')
+      }
+    },
+    onSuccess: () => {
+      setError('')
+      navigate({ to: '/' })
+    },
+    onError: () => {
+      setError('Invalid email or password')
+    },
+  })
+}
